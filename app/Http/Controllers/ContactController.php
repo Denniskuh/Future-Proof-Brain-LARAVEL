cf52d54e22572fb4e054be13dfcc15f69b554e06
Dennis Ego <dennis.ego@hotmail.be>
2021-03-09 13:59:53 +0100

    initial commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,11 +7,13 @@ use Mail;
 
 class ContactController extends Controller
 {
+    //MAKE THE FORM
     public function createForm(Request $request)
     {
         return view('contact');
     }
 
+    //VALIDATE AND SEND THE EMAIL
     public function contactUs(Request $request)
     {
         //Form validation
@@ -24,11 +26,10 @@ class ContactController extends Controller
         //  Send mail to admin
         \Mail::send('mail', array(
             'email' => $request->get('email'),
-            'subject' => 'Message from website',
             'user_query' => $request->get('message'),
         ), function($message) use ($request){
             $message->from($request->email);
-            $message->to('dennis.ego@hotmail.be', 'Admin')->subject($request->get('subject'));
+            $message->to('dennis.ego@hotmail.be', 'Dennis Ego')->subject('Message from website');
         });
 
         return back()->with('success', 'Bedankt om ons te contacteren! We connecteren je zo snel mogelijk met de juiste persoon.');

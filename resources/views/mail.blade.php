82216f7e2c661a5dc33f2f9cad7034b79385f969
Dennis Ego <dennis.ego@hotmail.be>
2021-03-09 15:32:12 +0100

    initial commit

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -22,50 +22,47 @@
             }
         }
     </style>
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;"><tr>
-<td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-<tr>
-<td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
-        <a href="https://markdownmail.com" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 #ffffff;">
-            Future Proof Brain
-        </a>
-    </td>
-</tr>
-<!-- Email Body --><tr>
-<td class="body" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; border-bottom: 1px solid #edeff2; border-top: 1px solid #edeff2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-                            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
-<!-- Body content --><tr>
-<td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                                        <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">Heading 1</h1>
-<p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">This is a paragraph filled with Lorem Ipsum and a link. Cumque dicta <a href="" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869d4;">doloremque eaque</a>, enim error laboriosam pariatur possimus tenetur veritatis voluptas.</p>
-<h2 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 16px; font-weight: bold; margin-top: 0; text-align: left;">Heading 2</h2>
-<div class="table" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-<table style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-    <thead style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;"><tr>
-        <th>Je hebt een bericht ontvangen van : {{ $email }}</th>
-    </thead>
-    <tbody style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-        User details: <br><br>
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;"><tr>
+        <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                        <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+        <tr>
+        <td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
+                <a href="https://markdownmail.com" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #ffffff; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 #ffffff;">
+                    Future Proof Brain
+                </a>
+            </td>
+        </tr>
+        <!-- Email Body --><tr>
+        <td class="body" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; border-bottom: 1px solid #edeff2; border-top: 1px solid #edeff2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+                                    <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+            <!-- Body content --><tr>
 
-        Email: {{ $email }} <br>
-        Onderwerp: {{ $subject }} <br>
-        Bericht: {{ $user_query }} <br><br>
-    </tbody>
-</table>
+            <div class="table" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+            <table style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+                <thead style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;"><tr>
+                    <th>Je hebt een bericht ontvangen van : {{ $email }}</th>
+                </thead>
+                <tbody style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                    User details: <br><br>
 
-<tr>
-<td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-    <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;"><tr>
-        <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-            <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #aeaeae; font-size: 12px; text-align: center;">© Fututre Proof Brain</p>
+                    Email: {{ $email }} <br>
+                    Onderwerp: {{ $subject }} <br>
+                    Bericht: {{ $user_query }} <br><br>
+                </tbody>
+            </table>
+
+            <tr>
+            <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;"><tr>
+                    <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+                        <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #aeaeae; font-size: 12px; text-align: center;">© Fututre Proof Brain</p>
+                    </td>
+                </tr>
+            </table>
+            </td>
+            </tr>
+            </table>
         </td>
-    </tr>
-</table>
-</td>
-</tr>
-</table>
-</td>
-        </tr></table>
+    </table>
 </body>
 </html>

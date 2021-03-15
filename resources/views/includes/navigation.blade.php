fff1f349cca304e4756397abf14d04e37fadbc2c
Dennis Ego <dennis.ego@hotmail.be>
2021-03-15 17:02:16 +0100

    server update

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,9 +1,9 @@
 <div class="sidebar bg--neutrals-1">
     <ul class="sidebar-ul">
-        <li><a href="/home" class="ff--normal fw--bold fc--neutrals-0">Home</a></li>
-        <li><a href="/ons-team" class="ff--normal fw--bold fc--neutrals-0">Ons team</a></li>
-        <li><a href="#" class="ff--normal fw--bold fc--neutrals-0">Projecten</a></li>
-        <li><a href="/contact" class="ff--normal fw--bold fc--neutrals-0">Contact</a></li>
+        <li><a href="/home" class="ff--normal fw--bold fc--neutrals-0 fs--size-3">Home</a></li>
+        <li><a href="/ons-team" class="ff--normal fw--bold fc--neutrals-0 fs--size-3">Ons team</a></li>
+        <li><a href="#" class="ff--normal fw--bold fc--neutrals-0 fs--size-3">Projecten</a></li>
+        <li><a href="/contact" class="ff--normal fw--bold fc--neutrals-0 fs--size-3">Contact</a></li>
     </ul>
     <button class="sidebarBtn bg--neutrals-0">
         <span class="bg--neutrals-1"></span>

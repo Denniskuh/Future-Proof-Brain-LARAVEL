063cc515d4a490c3e10e8bd0cad592ff56a069ae
Dennis Ego <dennis.ego@hotmail.be>
2021-09-02 20:54:15 +0200

    GA

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html lang="nl">
     <head>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-206591731-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-206591731-1');
+        </script>
+
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Future Proof Brain - {{$title}}</title>  
